e687232b6bc7c711970ae04895c223de78358c20
Anthony Bonello <ab@anthonybonello.co.uk>
2018-02-26 13:09:42 +0000

    Got pads to light up properly in sequence. I need to clean up code.

--- js/code.js
+++ js/code.js
@@ -1,6 +1,6 @@
 var gameList = [];
 var padId = 0, color, level = 0;
-var delayTime = 700; // ms to wait before switching back to normal pad color.
+var delayTime = 2000; // ms to wait before switching back to normal pad color.
 $(document).ready(function() {
     $(".start").click(function(){
         level++;
@@ -13,24 +13,39 @@ function doGameList(){
     $(".level").text(level);
     //generate random number and add it to gameList
     giveMeRandomNumber();
-    padId = gameList[gameList.length - 1];
-    console.log("#pad"+padId);
+    // padId = gameList[gameList.length - 1];
+    // console.log("#pad"+padId);
     
     // get the color
-    color = $("#pad"+padId).attr("class").split(" ")[1];
-    console.log("#pad"+padId+" : "+color);
-    
-    // set lighter color class for pad
-    $("#pad"+padId).addClass("light"+color).removeClass(color);
-    // reset color for pad
-    setInterval(function() {
-        $("#pad"+padId).addClass(color).removeClass("light"+color);
-    }, delayTime); // Do this after time set by delayTime.
-    
+    // color = $("#pad"+padId).attr("class").split(" ")[1];
+    // console.log("#pad"+padId+" : "+color);
+    var x = 0;
+    var sequence = setInterval(function() {
+        padId = gameList[x];
+        console.log("#pad"+padId);
+        color = $("#pad"+padId).attr("class").split(" ")[1];
+        console.log("#pad"+padId+" : "+color);
+        
+        // set lighter color class for pad
+        $("#pad"+padId).addClass("light"+color).removeClass(color);
+        // reset color for pad
+        // setInterval(function() {
+        setTimeout(function() {
+            $("#pad"+padId).addClass(color).removeClass("light"+color);
+        }, delayTime); // Do this after time set by delayTime.
+        console.log(gameList[x]);
+        x++;
+        if (x == gameList.length) {
+            x = 0;
+            clearInterval(sequence);
+        }
+    }, 3000);
+        
+       
+    }
 
 
     
-}
 
 //generate random number
 function giveMeRandomNumber(){
